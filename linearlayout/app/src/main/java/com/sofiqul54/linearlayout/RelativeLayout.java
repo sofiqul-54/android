290789d1a951796a1c4bf2681412f534bce28005
sofiqul-54 <sofiqul54@gmail.com>
2019-03-25 00:51:38 +0600

    test

---
+++ linearlayout/app/src/main/java/com/sofiqul54/linearlayout/RelativeLayout.java
@@ -0,0 +1,13 @@
+package com.sofiqul54.linearlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RelativeLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative_layout);
+    }
+}
