91caff47c24fcb4a388dcf0d2cc50a8a928eb5fa
sofiqul-54 <sofiqul54@gmail.com>
2019-03-25 13:15:23 +0600

    android

---
+++ linearlayout/app/src/main/java/com/sofiqul54/linearlayout/Linear2.java
@@ -0,0 +1,13 @@
+package com.sofiqul54.linearlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Linear2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear2);
+    }
+}
